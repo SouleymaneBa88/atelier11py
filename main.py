3255e44f4d7cabcd869614de1a69c1c752e7f9af
Souleymane Ba <sbjule2180@gmail.com>
2026-01-06 09:39:16 +0000

    correction

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ depenses = []
 
 def afficher_menu():
     print("\n --Gestion de depense--")
-    print("1. Ajouter une description ")
-    print("2. Ajouter une dépense par catégorie")
-    print("3. Afficher les dépenses")
-    print("4. Quitter")
+    # print("1. Ajouter une description ")
+    print("1. Ajouter une dépense par catégorie")
+    print("2. Afficher les dépenses")
+    print("3. Quitter")
 
 
-def message(depenses):
-    while True:
-        description = str(input("Décris tes dépenses : "))
-        if description.isalpha() and description.isnumeric():
-            print("cool")
-            break
-        else:
-            print("Veuillez une description correct de votre depense")
+# def message(depenses):
+#     while True:
+#         description = str(input("Décris tes dépenses : "))
+#         if description.isalpha() :
+#             print("cool")
+#             break
+#         else:
+#             print("Veuillez une description correct de votre depense")
 
     # while True:
     #     montant_str = input("Montant : ")
@@ -27,18 +27,21 @@ def message(depenses):
     #         else:
     #             print("Montant incorrect, veuillez resaisir !")
     #     else:
-    depenses.append({"description": description})
-    print("Dépense bien ajoutée")
+    # depenses.append({"description": description})
+    # print("Dépense bien ajoutée")
 
 
 def add_categorie(depenses):
+    stockage=['vetement', 'nouriture','transport']
+
     for i in range(1, 4):
         while True:
-            categorie_input = str(input(f"Type de produit {i} : "))
-            if categorie_input.isalpha():
-                break
-            else:
-                print("Le type de produit ne peut pas être vide.")
+            for i ,stock in enumerate(stockage):
+                categorie_input = (input(f"Type de produit {i} : "))
+                if categorie_input == stockage:
+                    break
+                else:
+                    print("Le type de produit ne peut pas être vide.")
         while True:
             prix_categorie = input(f"Prix pour {categorie_input} : ")
             if prix_categorie.replace('.', '', 1).isdigit() and prix_categorie.count('.') <= 1:
@@ -49,6 +52,15 @@ def add_categorie(depenses):
                     print("Prix incorrect, veuillez resaisir !")
             else:
                 print("Prix invalide, veuillez entrer un nombre valide.")
+        while True:
+            description = str(input("Décris tes dépenses : "))
+            if description.isalpha() :
+                print("cool")
+                # message['message']=description
+                break
+            else:
+                print("Veuillez une description correct de votre depense")
+        # depenses.append(message)
         depenses.append({"categorie": categorie_input, "montant": prix_categorie})
     print("Dépenses par catégorie ajoutées")
 
@@ -61,13 +73,13 @@ def afficher_depenses(depenses):
     print("\nListe de toutes les dépenses :")
     total = 0
     categories = {}
-    for i, dep in enumerate(depenses,1):
-        prix_categorie = dep.get("montant")
+    for i, depense in enumerate(depenses,1):
+        prix_categorie = depense.get("montant")
         total += prix_categorie
-        if "description" in dep:
-            print(f"{i}. {dep['description']} : {prix_categorie} Fcf")
-        elif "categorie" in dep:
-            categorie = dep["categorie"]
+        if "description" in depense:
+            print(f"{i}. {depense['description']} : {prix_categorie} Fcf")
+        elif "categorie" in depense:
+            categorie = depense["categorie"]
             print(f"{i}. Catégorie { categorie} : {prix_categorie} Fcf")
             if  categorie not in categories:
                 categories[ categorie] = 0
@@ -93,12 +105,15 @@ while True:
                 print("Choix invalide, veuillez choisir entre 1 et 4.")
         except ValueError:
             print("Veuillez entrer un nombre valide.")
+    # if choix == 1:
+    #     message(depenses)
     if choix == 1:
-        message(depenses)
-    elif choix == 2:
         add_categorie(depenses)
-    elif choix == 3:
+    elif choix == 2:
         afficher_depenses(depenses)
-    elif choix == 4:
+    elif choix == 3:
         print("Au revoir !")
+   
+    # elif choix == 4:
+    #     print("Au revoir !")
         break
